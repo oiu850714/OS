ae7e93c35e73827e469f4651b9f3c23d57090f24
oiu850714 <oiu850714@gmail.com>
2016-12-01 12:18:48 +0800

    almost complete HW4.... with low probability that will run on single core

--- HW4/0317001.cpp
+++ HW4/0317001.cpp
@@ -22,8 +22,7 @@ struct array
 
 queue<int> job_queue;
 
-int shared_array[1500000];
-int shared_array2[1500000];
+int *shared_array;
 sem_t sem[16];
 //!!!!!!!!!!!!!!!!!!!! sem[0] is for round!!!!!!!!!!!!!!
 sem_t call_main[8];
@@ -34,6 +33,7 @@ array Array[16];
 sem_t mutex_job_queue;
 sem_t mutex_start_job;
 
+void merge(int* begin_1, int* end_1, int* begin_2, int* end_2);
 
 void* thread_HW4(void *p);
 //thread's entry point
@@ -64,7 +64,7 @@ void switch_output_size(int num_of_elements, int pool_size);
 int main(int argc, char const *argv[])
 {
 
-	for(int pool_size = 8; pool_size >= 1; pool_size--)
+	for(int pool_size = 1; pool_size <= 8; pool_size++)
 	{
 		sem_init(&mutex_job_queue, 0, 1);
 		sem_init(&mutex_start_job, 0, 0);
@@ -78,7 +78,8 @@ int main(int argc, char const *argv[])
 
 	    int num_of_elements;
 		fscanf(fp, "%d", &num_of_elements);
-		//scan number of elements 
+		//scan number of elements
+		shared_array = (int*)malloc(num_of_elements * sizeof(int));
 
 		for(int i = 0; i < num_of_elements; ++i)
 		{
@@ -143,11 +144,16 @@ int main(int argc, char const *argv[])
 		for(int i = 0; i < 8; ++i)
 		{
 			sem_wait(call_main + i);
-			printf("job %d finished!!\n", i + 8);
+			//HW4 not print: printf("job %d finished!!\n", i + 8);
 		}
 		// HW4: main in this HW4 may need another way
 		// to check that array is sorted
 
+		for(int i = 9; i <= 15; i++)
+		{
+			//HW4 not print: printf("merge %dth!!!!!!!!!!\n", i-8);
+			merge(Array[8].begin, Array[i-1].end, Array[i].begin, Array[i].end);
+		}
 		//printf("main thread finish waiting 8~15 thread!!\n");
 	    
 	    timeval end;
@@ -159,7 +165,7 @@ int main(int argc, char const *argv[])
 	    printf("Elapsed time: %f ms \n", (sec*1000+(usec/1000.0)));
 		switch_output_size(num_of_elements, pool_size);
 
-		printf("poolsize %d finished\n", pool_size);
+		//HW4 not print: printf("poolsize %d finished\n", pool_size);
 		for(int i = 0; i < pool_size; i++)
 		{
 			pthread_cancel(*(tid+i));
@@ -168,6 +174,7 @@ int main(int argc, char const *argv[])
 		{
 			Array[i].begin = Array[i].end = NULL;	
 		}
+		free(shared_array);
 	}
 	return 0;
 }
@@ -194,15 +201,16 @@ void* thread_HW4(void *p)
 			int empty_flag = 0;
 			if(Array[round].begin == Array[round].end)
 			{
-				printf("job %d empty!!!\n", round);
+				//HW4 not print: printf("job %d empty!!!\n", round);
 				Array[2*round].begin = Array[2*round].end = Array[round].begin;
 				Array[2*round+1].begin = Array[2*round+1].end = Array[round].begin;
 				empty_flag =1;
 			}
 			if(!empty_flag)
 			{
-				int *pivot = select_pivot(Array[round].begin, Array[round].end);
-				printf("round : %d pivot: %d begin: %d end: %d\n", round, pivot, Array[round].begin, Array[round].end );
+				int *pivot = Array[round].begin + (Array[round].end - Array[round].begin)/2;
+				//int *pivot = select_pivot(Array[round].begin, Array[round].end);
+				//HW4 not print: printf("round : %d pivot: %d begin: %d end: %d\n", round, pivot, Array[round].begin, Array[round].end );
 				//printf("thread %d select finished\n", round);
 				//printf("%d pivot!!!\n", *pivot);
 				//select pivot and signal such that child can sort
@@ -210,29 +218,17 @@ void* thread_HW4(void *p)
 				Array[2 * round].end = pivot;
 				//sem_post(sem + 2 * round);
 				//HW4: I think line above is not reqeired.
-				Array[2 * round + 1].begin = pivot + 1;
+				Array[2 * round + 1].begin = pivot;
 				Array[2 * round + 1].end = Array[round].end;
 				//sem_post(sem + 2 * round + 1);
 				//HW4: I think line above is not reqeired.
 			}
-			//printf("%d really has sorted array\n", round);
-		}
-		else
-		{
-			printf("job %d start sorting\n", round );
-			bubble_sort (Array[round].begin, Array[round].end);
-			//printf("thread %d really sorting\n", round);
-			sem_post(call_main + round - 8);
-			//use lib sort and signal such that mother thread can report
-		}
-		if(round < 8)
-		{
 			sem_wait(&mutex_job_queue);
 			job_queue.push(2*round);
 			job_queue.push(2*round + 1);
-			sem_post(&mutex_start_job);
-			sem_post(&mutex_start_job);
 			sem_post(&mutex_job_queue);
+			sem_post(&mutex_start_job);
+			sem_post(&mutex_start_job);
 			//must put above lines here, otherwise..
 			//there maybe this case:
 			//thread_A and thread_B will push round, A pushes before B,
@@ -245,7 +241,33 @@ void* thread_HW4(void *p)
 			//printf("job %d start pushing new job %d\n", round, 2*round+1);
 			//sem_post(&mutex_start_job);
 			//sem_post(&mutex_job_queue);
-			printf("job %d push new jobs in queue!\n",round );
+			//HW4 not print: printf("job %d push new jobs in queue!\n",round );
+			//printf("%d really has sorted array\n", round);
+		}
+		else
+		{
+			//HW4 not print: printf("job %d start sorting\n", round );
+			/*
+			int *tmp_array = (int*)malloc((Array[round].end - Array[round].begin) * sizeof(int));
+			for(int i = 0; i < (Array[round].end - Array[round].begin); i++)
+			{
+				tmp_array[i] = *(Array[round].begin + i);
+			}
+			bubble_sort(tmp_array, tmp_array + (Array[round].end - Array[round].begin));
+			//printf("thread %d really sorting\n", round);
+			for (int i = 0; i < (Array[round].end - Array[round].begin); ++i)
+			{
+				*(Array[round].begin + i) = tmp_array[i];
+			}
+			free(tmp_array);
+			*/
+			bubble_sort(Array[round].begin, Array[round].end);
+			sem_post(call_main + round - 8);
+			//use lib sort and signal such that mother thread can report
+		}
+		if(round < 8)
+		{
+			
 		}
 	}
 }
@@ -298,8 +320,43 @@ void swap(int* const first, int* const second)
 int* select_pivot(int *begin, int *end)
 {
 	int * const pivot = end - 1;
+	int * pindex = begin;
+	if(end - begin >= 3)
+	{
+		int a = *(end-1), b = *(end-2), c = *(end-3);
+		int first, second, third;
+		if(a < b)
+		{
+			third = a;
+			second = b;
+		}
+		else
+		{
+			second = a;
+			third = b;
+		}
+		if(second < c)
+		{
+			first = c;
+		}
+		else
+		{
+			first = second;
+			if(third < c)
+			{
+				second = c;
+			}
+			else
+			{
+				second = third;
+				third = c;
+			}
+		}
+		*(end - 1) = second;
+		*(end - 2) = third;
+		*(end - 3) = first;
+	}
 	//the last element is *(end -1);
-	int *pindex = begin;
 	for(int *i = begin; i != end -1; ++i)
 	{
 		if(*i <= *pivot)
@@ -343,7 +400,7 @@ void cal_time(int num_of_elements, int pool_size)
 	int sec,usec;
 	timeval start, end;
 	gettimeofday(&start, 0);
-    bubble_sort(shared_array, shared_array + num_of_elements);
+    sort(shared_array, shared_array + num_of_elements);
     gettimeofday(&end, 0);
     sec = end.tv_sec - start.tv_sec;
     usec = end.tv_usec - start.tv_usec;
@@ -362,6 +419,8 @@ void bubble_sort(int *begin, int *end)
 			if(*(begin+j) > *(begin+j+1))
 			{
 				swap(begin+j, begin+j+1);
+				//counter++;
+				//counter == 100 ? printf("%d",round), counter = 0 : false;
 			}
 		}
 	}
@@ -395,4 +454,48 @@ void switch_output_size(int num_of_elements, int pool_size)
 			output_file(num_of_elements, "output_8.txt");
 			break;
 		}
+}
+
+
+void merge(int* begin_1, int* end_1, int* begin_2, int* end_2)
+{
+	int length_l = end_1 - begin_1;
+	int length_r = end_2 - begin_2;
+	int i = 0, j = 0, k = 0;
+	int* tmp_array = (int*)malloc(sizeof(int) *( length_l + length_r ) );
+	while(i < length_l && j < length_r)
+	{
+		if( begin_1[i] <= begin_2[j])
+		{
+			tmp_array[k] = begin_1[i++];
+		}
+		else
+		{
+			tmp_array[k] = begin_2[j++];
+		}
+		++k;
+	}
+	while(i < length_l)
+	{
+		tmp_array[k] = begin_1[i++];
+		++k;
+	}
+	while(j < length_r)
+	{
+		tmp_array[k] = begin_2[j++];
+		++k;
+	}
+	for(int i = 0; i < length_l; i++)
+	{
+		begin_1[i] = tmp_array[i];
+		//HW4 not print: printf("%d ", begin_1[i]);
+	}
+	//HW4 not print: printf("\n");
+	for(int i = 0; i < length_r; i++)
+	{
+		begin_2[i] = tmp_array[length_l+i];
+		//HW4 not print: printf("%d ", begin_2[i]);
+	}
+	//HW4 not print: printf("\n");
+	free(tmp_array);
 }
